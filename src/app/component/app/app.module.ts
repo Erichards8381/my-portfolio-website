8e288941b4f3b9c8f2194ae97adc123d58c53211
Erichards8381 <74271254+Erichards8381@users.noreply.github.com>
2023-04-30 15:25:25 -0400

    added hash location strategy

--- src/app/component/app/app.module.ts
+++ src/app/component/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from '../navbar/navbar.component';
@@ -38,7 +38,7 @@ import { FullScreenImageDirective } from 'src/app/directives/full-screen-image.d
     AppRoutingModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
