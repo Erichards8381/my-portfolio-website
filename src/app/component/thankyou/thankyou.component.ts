d8a7c379afa54c1231a0d2d82fbb0cba5154e03e
Erichards8381 <74271254+Erichards8381@users.noreply.github.com>
2023-04-30 19:59:09 -0400

    even more debugging added

--- src/app/component/thankyou/thankyou.component.ts
+++ src/app/component/thankyou/thankyou.component.ts
@@ -13,16 +13,17 @@ export class ThankyouComponent {
   }
 
   ngOnInit(): void {
-    console.log('Hello from thankyou component')
     const contactFormSubmitted = localStorage.getItem('contactFormSubmitted') || document.cookie.includes('contactFormSubmitted=true')
     if (!contactFormSubmitted) {
       this.router.navigateByUrl('')
     }
     else{
+      console.log('contactFormSubmitted cookie value pre removal:', document.cookie);
       localStorage.removeItem('contactFormSubmitted');
       document.cookie = 'contactFormSubmitted=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;';
       console.log(localStorage.getItem('contactFormSubmitted'))
       console.log(document.cookie.includes('contactFormSubmitted=true'))
+      console.log('contactFormSubmitted cookie value post removal:', document.cookie);
     }
   }
 }
